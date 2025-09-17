ad3f3b8b0069dc9ec9095db6e4727f5bf0242b2b
Amos Ombworo <amosombworo@gmail.com>
2025-09-17 13:52:01 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
+
+console.log("Company Name:");
+console.log(companyName);
+
+console.log("Most Profitable Neighborhood:");
+console.log(mostProfitableNeighborhood);
+
+console.log("Company CEO:");
+console.log(companyCeo);
